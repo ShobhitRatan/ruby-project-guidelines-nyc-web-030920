54165591d57e15ed26013929b55856e3553791a4
github username <github email address>
2020-04-07 19:49:14 -0400

    commit

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,8 @@
 class Team < ActiveRecord::Base 
-
+    has_many :current_seasons 
+    has_many :competitions, through: :current_seasons 
+    has_many :matches, through: :current_seasons 
+    has_many :seasons, through: :current_seasons 
+    has_many :lineups 
+    has_many :players, through: :lineups 
 end 
