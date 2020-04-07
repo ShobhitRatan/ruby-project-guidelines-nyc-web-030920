4f061349e528528462c11e0a568c650fd344a390
github username <github email address>
2020-04-07 19:27:57 -0400

    commit

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,6 @@
 class Competition < ActiveRecord::Base 
-
+    has_many :current_seasons 
+    has_many :seasons, through: :current_seasons 
+    has_many :teams, through: :current_seasons 
+    has_many :matches, through: :current_seasons 
 end 
