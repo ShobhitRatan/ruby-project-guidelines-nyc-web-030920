fdf4a24cede9be042662f716308747930cc0852a
github username <github email address>
2020-04-08 15:33:48 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 Competition.destroy_all 
  
-
-c1 = Competition.new(name: name, plan: plan)  
+competitions = GetRequester.new('http://api.football-data.org/v2/competitions/') 
+competitions.get_response_body
+ competitions.parse_json.each do |name| 
+    Competition.create(name, name.data) 
+end  
+ 
+competitions.parse_json.each do |plan| 
+    Competition.create(plan, plan.data) 
+end 
+ 
+c1 = Competition.create(name: "name", plan: "plan")   
 
