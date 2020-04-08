3e1327045676a6538293322f6ac8511dd7da869c
github username <github email address>
2020-04-08 15:42:22 -0400

    commit

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,4 @@ require_relative '../config/environment'
 
 
 puts "HELLO WORLD"
-competitions = GetRequester.new('http://api.football-data.org/v2/competitions/') 
-competitions.get_response_body
-competitions.parse_json
+
