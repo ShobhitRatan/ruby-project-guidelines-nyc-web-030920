5cf47cd491dcbfa21f9a88558d3f9fa4980db4fe
github username <github email address>
2020-04-08 17:45:13 -0400

    Reinstated the c1 test data before trying to read the csv file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
 
 Competition.destroy_all 
 
-competitions = GetRequester.new('http://api.football-data.org/v2/competitions/')
-competitions.get_response_body 
-competitions.parse_json.each do |competition| 
-    Competition.create(name: competition.name, plan: competition.plan)  
-end 
+c1 = Competition.create(name: "Premier League", plan: "TIER ONE") 
