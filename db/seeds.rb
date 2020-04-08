3fbacda771655a16b8c0a2ffc153d779eb8150c6
github username <github email address>
2020-04-08 15:22:19 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 Competition.destroy_all 
-CurrentSeason.destroy_all 
-Lineup.destroy_all 
-Match_day.destroy_all 
-Match.destroy_all 
-Player.destroy_all 
-Season.destroy_all 
-Team.destroy_all 
+ 
 
 
 competitions = GetRequester.new('http://api.football-data.org/v2/competitions/') 
