741ff69883ef0e333425181b301679781674cf63
github username <github email address>
2020-04-08 15:34:02 -0400

    commit

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,15 +3,4 @@ class Competition < ActiveRecord::Base
     has_many :seasons, through: :current_seasons 
     has_many :teams, through: :current_seasons 
     has_many :matches, through: :current_seasons 
-
-    def self.name 
-        competitions.parse_json.each do |name| 
-            Competition.create(name, name.data) 
-        end  
-    end 
-    def self.plan 
-        competitions.parse_json.each do |plan| 
-            Competition.create(plan, plan.data) 
-        end 
-    end 
 end 
