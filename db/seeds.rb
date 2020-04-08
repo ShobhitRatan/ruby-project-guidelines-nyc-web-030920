57268ae6ce42828b7e4eb51bc074aa3d10d9d4f1
github username <github email address>
2020-04-08 15:41:50 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
 Competition.destroy_all 
  
-competitions = GetRequester.new('http://api.football-data.org/v2/competitions/') 
-competitions.get_response_body
- competitions.parse_json.each do |name| 
-    Competition.create(name, name.data) 
-end  
- 
-competitions.parse_json.each do |plan| 
-    Competition.create(plan, plan.data) 
-end 
- 
-c1 = competitions    
-
+c1 = Competiton.create(name: "Premier League", plan: "TIER ONE") 
