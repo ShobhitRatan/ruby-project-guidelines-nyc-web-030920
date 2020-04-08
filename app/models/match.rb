1b8a117a4ce1b34bf93ffc9c6d84ff7d51d415f8
github username <github email address>
2020-04-08 16:55:05 -0400

    commit

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,4 @@
-class Match < ActiveRecord 
+class Match < ActiveRecord::Base
     has_many :current_seasons 
     has_many :competitions, through: :current_seasons 
     has_many :seasons, through: :current_seasons 
--- app/test.rb
+++
@@ -1,3 +0,0 @@
-def jeff 
-    puts "dope"
-end 
