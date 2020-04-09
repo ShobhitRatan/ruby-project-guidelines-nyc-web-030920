2d3b3e32bc8f664708904a6f4955eabeb399163a
github username <github email address>
2020-04-09 14:07:21 -0400

    Method to add a competition.

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -3,4 +3,9 @@ class Competition < ActiveRecord::Base
     has_many :seasons, through: :current_seasons 
     has_many :teams, through: :current_seasons 
     has_many :matches, through: :current_seasons 
+    
+    # Method to add a new competition. 
+    def add_competition(name, plan, country)
+        Competition.create(name: name, plan: plan, country: country) 
+    end 
 end 
