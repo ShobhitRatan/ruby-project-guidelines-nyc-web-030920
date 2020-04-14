23c47333f876f4bc00d4fad80a62ee1941d68cdc
github username <github email address>
2020-04-14 12:32:36 -0400

    commit

--- app/models/command_line_interface.rb
+++ app/models/command_line_interface.rb
@@ -1,5 +1,18 @@
 class CommandLineInterface 
     def greet 
         puts "Welcome to Soccer Data Exchange! The go to place for soccer fans from around the world." 
-    end
+    end 
+    def options 
+        puts "1- TIER ONE" 
+        puts "2- TIER TWO" 
+        puts "3- TIER THREE" 
+        puts "4- TIER FOUR" 
+    end 
+    def run 
+        greet 
+        puts "Let's pick a competition from one of the four options: "  
+        choice = gets.chomp(options) 
+        Competition.find_by(plan: choice) 
+    end 
+
 end  
