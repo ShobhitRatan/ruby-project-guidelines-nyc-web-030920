f6a8b1579c45ea2754882a2af351b1c91773017e
github username <github email address>
2020-04-16 16:20:24 -0400

    commit

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,16 +4,4 @@ class Competition < ActiveRecord::Base
     has_many :teams, through: :current_seasons 
     has_many :matches, through: :current_seasons 
     belongs_to :competition_plan 
-    # Method to add a new competition. 
-    def self.add_competition(name, plan, country)
-        Competition.create(name: name, plan: plan, country: country) 
-    end 
-    def self.remove_competition(name) 
-        Competition.find_by(name) 
-        Competition.destroy(name)
-    end 
-    def self.update_competition(old_name, new_name) 
-        competition_name = Competition.find_by(old_name) 
-        competition_name.update(name: new_name) 
-    end 
 end 
