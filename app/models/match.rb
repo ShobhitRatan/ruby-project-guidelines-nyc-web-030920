b028090801ed8df65f015c56ac79a9002037ae0b
github username <github email address>
2020-04-16 22:54:20 -0400

    commit

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,9 @@
 class Match < ActiveRecord::Base
     has_many :current_seasons 
     has_many :competitions, through: :current_seasons 
-    has_many :seasons, through: :current_seasons 
     has_many :teams, through: :current_seasons 
     has_many :match_days 
+    belongs_to :season 
     def self.group_a_teams 
         Match.teams.where("group = Group A").all
     end 
--- app/models/season.rb
+++ app/models/season.rb
@@ -2,5 +2,5 @@ class Season < ActiveRecord::Base
     has_many :current_seasons 
     has_many :competitions, through: :current_seasons  
     has_many :teams, through: :current_seasons 
-    has_many :matches, through: :current_seasons 
+    has_many :matches 
 end 
