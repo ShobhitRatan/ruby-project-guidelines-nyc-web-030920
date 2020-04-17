15ae62836ce9deaec771edcba7406cada11a1a1d
github username <github email address>
2020-04-16 22:58:42 -0400

    commit

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,7 @@
 class Match < ActiveRecord::Base
     has_many :current_seasons 
     has_many :competitions, through: :current_seasons 
+    has_many :seasons, through: :current_seasons 
     has_many :teams, through: :current_seasons 
     has_many :match_days 
     belongs_to :season 
